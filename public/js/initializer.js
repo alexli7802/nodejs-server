02e4751830391fcbd026e9f916e22638be380f22
alex.li <alex.xin.li@hotmail.com>
2018-08-10 16:31:17 +1000

    trying bar chart

--- public/js/initializer.js
+++ public/js/initializer.js
@@ -1,6 +1,15 @@
 
+var chartdata = [40,64,120,78,180,91]
 
 function initializeHomePage() {
   // document.write("This home page is still under developing!");
   console.log("initializeHomePage is called")
+  d3.select('body')
+    .append('svg').attr('width',720).attr('height',200)
+    .selectAll('rect').data(chartdata)
+    .enter().append('rect')
+    .style({'fill':'#3c763d','stroke':'#d6e9c6','stroke-width':'5'})
+    // .attr('width': 40).attr('height', function(data) { return data; })
+    // .attr('x', function(data,i) { return i * (40 + 20); })
+    // .attr('y', function(data) { return 200 - data })
 }
